c221cc43c35187a2a29577ffbc36cad6a82c4674
Brigita <brigita.rusan@gmail.com>
2025-03-17 12:16:36 +0100

    adding files

---
+++ Data/DbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using knjiznica.Models;
+
+namespace knjiznica.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options){}
+
+    }
+    public class KnjiznicaContext : DbContext
+    {
+        public KnjiznicaContext(DbContextOptions<KnjiznicaContext> options) : base(options) { }
+
+        public DbSet<KnjiznicarModel> Knjiznicari { get; set; }
+        public DbSet<KnjiznicaModel> Knjiznice { get; set; }
+        public DbSet<KnjigaModel> Knjige { get; set; }
+        public DbSet<KorisnikModel> Korisnici { get; set; }
+        public DbSet<KnjiznicaKnjigaModel> KnjiznicaKnjige { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<KnjiznicarModel>().ToTable("Knjiznicar");
+            modelBuilder.Entity<KnjiznicaModel>().ToTable("Knjiznica");
+            modelBuilder.Entity<KnjigaModel>().ToTable("Knjige");
+            modelBuilder.Entity<KorisnikModel>().ToTable("Korisnici");
+            modelBuilder.Entity<KnjiznicaKnjigaModel>().ToTable("Knjiz_knjige");
+
+            modelBuilder.Entity<KnjiznicarModel>()    // knjiznica - knjiznicar
+                .HasOne(k => k.Knjiznica)
+                .WithMany(kj => kj.Knjiznicari)
+                .HasForeignKey(k => k.Id_knjiznice);
+
+            modelBuilder.Entity<KnjiznicaKnjigaModel>()
+                .HasKey(k => new { k.Id_knjige, k.Id_knjiznice });  // Kombinirani ključ za pomoćnu tablicu
+
+            modelBuilder.Entity<KnjiznicaKnjigaModel>()  
+                .HasOne(k => k.Knjiga)
+                .WithMany(k => k.KnjizniceKnjige)
+                .HasForeignKey(k => k.Id_knjige);
+
+            modelBuilder.Entity<KnjiznicaKnjigaModel>()
+                .HasOne(k => k.Knjiznica)
+                .WithMany(k => k.KnjizniceKnjige)
+                .HasForeignKey(k => k.Id_knjiznice);
+        }
+
+
+    }
+}
---
+++ Migrations/20250313203725_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace knjiznica.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Knjige",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Naziv = table.Column<string>(type: "TEXT", nullable: false),
+                    Autor = table.Column<string>(type: "TEXT", nullable: false),
+                    Isbn = table.Column<string>(type: "TEXT", nullable: false),
+                    Kolicina = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Knjige", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Knjige");
+        }
+    }
+}
